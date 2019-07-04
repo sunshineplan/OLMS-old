10c3c13f20e0f0b0e71ecea9f2152769efb1a718
sunshineplan <sunshineplan@gmail.com>
2019-07-04 20:57:52 +0800

    Update py2exe.bat

--- py2exe.bat
+++ py2exe.bat
@@ -1,4 +1,4 @@
 @echo off
 pip install pyinstaller -U > nul
-pyinstaller -F --add-data OLMS\templates;templates --add-data OLMS\schema.sql;templates main.py -i main.ico
+pyinstaller -F --add-data OLMS\templates-zh;templates --add-data OLMS\schema.sql;templates main.py -i main.ico
 pause
